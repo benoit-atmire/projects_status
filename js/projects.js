28d86030e1790d6bcf892a39dd23b1d10584456d
benoit <benoit@atmire.com>
2018-10-23 21:25:34 +0200

    try to isolate failing call

--- js/projects.js
+++ js/projects.js
@@ -90,8 +90,9 @@ var updateBoard = function (t) {
             var labels = boarddata[1];
             console.log(JSON.stringify(labels, null, '\t'));
             console.log("Step 2 end");
-            return Promise.all([settings, t.cards('all'), t.lists('all)'), getProjects(settings.pm, settings.username, settings.password), labels]);
-        })
+            //return Promise.all([settings, t.cards('all'), t.lists('all)'), getProjects(settings.pm, settings.username, settings.password), labels]);
+            return Promise.all([t.cards('all'), t.lists('all)'), getProjects(settings.pm, settings.username, settings.password), labels]);
+        }, function (error) { console.error(error);})
     // Then process all that info
         .then(function (values) {
             console.log("Step 3 start");
