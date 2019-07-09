129f179ac3cd4033cb1da2ed608b78329a728cdd
benoit <benoit.wery@atmire.com>
2019-07-09 10:29:41 +0200

    separate updates and make them async

--- js/projects.js
+++ js/projects.js
@@ -21,7 +21,8 @@ TrelloPowerUp.initialize({
             width: 600
       });
     },
-    'card-badges': function(t, options) {
+    'card-badges': async function(t, options) {
+        await updateCard2(t);
         var b = getAllBadges(t, false); 
         console.log(b);
         return b;
@@ -48,7 +49,7 @@ function getAllBadges(t, detailed) {
             
                 // Plugindata contains all stored values in the card
                 //console.log(plugindata);
-                var settings = (plugindata.board && plugindata.board.private && plugindata.board.private.settings) ? plugindata.board.private.settings : {};// Shared settings to get data from the API
+//                var settings = (plugindata.board && plugindata.board.private && plugindata.board.private.settings) ? plugindata.board.private.settings : {};// Shared settings to get data from the API
                 var carddata = (plugindata.card && plugindata.card.shared) ? plugindata.card.shared : {};
                 var projectdata = carddata.project || {}; // Shared data that replicates and stores the content from the API
                 var sladata = carddata.sla || {}; // Shared data that contains the SLA details
@@ -67,14 +68,14 @@ function getAllBadges(t, detailed) {
 
                 var today = new Date();
 
-                if (!detailed) {
+/*                if (!detailed) {
                     var lastsyncdate = new Date(projectdata.week.value);                    
                     var lastweek = new Date(today.toISOString().substring(0,10)); // doing this in 2 steps prevents conflicts with hours being different
                     lastweek.setDate(lastweek.getDate()-(lastweek.getDay() || 7)-6); // we're interested only in weeks that are finished
 
-                    if (lastweek > lastsyncdate) projectdata = await updateCard(t, t.getContext().card, projectdata.pid.value, settings, plugindata.board.shared.labels);
+                    //if (lastweek > lastsyncdate) projectdata = await updateCard(t, t.getContext().card, projectdata.pid.value, settings, plugindata.board.shared.labels);
                 }
-                /* Now we can start generating the badges we need, being: 
+ */               /* Now we can start generating the badges we need, being: 
                 *   - an icon with a link to the project
                 *   - an icon with a link to the tracker if the project is an SLA and that the tracker has been included
                 *   - for fixed price projects, a counter of days left before next phase
@@ -135,6 +136,203 @@ function getAllBadges(t, detailed) {
                 // We're done with the badges, we can return them
                 return badges;
         });
+}
+
+async function updateCard2(t) {
+
+    // Retrieve the data from the board ...
+    var data = await Promise.all([t.getAll(), t.lists('all')]); // async call so we build the next steps on the resolved values
+
+    // ... and store them in conveniently named variables
+    var boarddata = data[0] || {};
+    var lists_table = data[1] || {}; 
+
+    var settings = (boarddata.board.private && boarddata.board.private.settings) ? boarddata.board.private.settings : {};
+    var project_data = (boarddata.card.shared && boarddata.card.shared.project) ? boarddata.card.shared.project : {}; 
+    var pid = project_data.pid ? project_data.pid.value : 0;
+
+    // If we don't have a pid, there's nothing much to do, so we can quit
+    if (!pid) return false;
+
+    // If card is already up-to-date, we can also quit
+    var lastsyncdate = new Date(projectdata.week.value);                    
+    var today = new Date();
+    var lastweek = new Date(today.toISOString().substring(0,10)); // doing this in 2 steps prevents conflicts with hours being different
+    lastweek.setDate(lastweek.getDate()-(lastweek.getDay() || 7)-6); // we're interested only in weeks that are finished
+
+    if (lastweek.getTime() <= lastsyncdate.getTime()) return false;
+
+    // In order to fine-tune the reports, let's also check whether this is a new item
+
+    var isNewCard = project_data.pm ? true : false;
+
+    // Else, we can start retrieving the latest data for the card
+    project_data = await getProjectData(pid, settings.apitoken);
+
+    // Transforming the list table into an object formatted to retrieve list IDs based on labels
+    var lists = {};
+    for (var i in lists_table){lists[lists_table[i].name] = lists_table[i].id;}    
+
+    // Let's store the current card_id in a variable so we don't have race conditions for parallel executions
+    var card_id = t.getContext().card;
+    // The function will need to return a Promise, as per Trello's Power Up rules, so let's embed all the processing withing a new promise
+    // Also we need to update the data stored in the card for this project
+                            
+    return Promise.all([t.set(card_id, 'shared', 'project', project_data), new Promise( function (resolve, reject){
+
+        // The base data for the card contains: (it overrides any previously existing data, if any, with the latest one)
+        var card = {
+            // The Trello API credentials
+            token: settings.ttoken,
+            key: settings.tkey,
+            // A description with the link to the W2P project
+            desc: "[W2P](https://web2project.atmire.com/web2project/index.php?m=projects%26a=view%26project_id=" + project_data.pid.value + ") %0D%0A",
+            // The card title is the proejct name and the client name
+            name: project_data.project_name.value + " (" + project_data.company_name.value + ")",
+            // And finaly the card is placed in the list that corresponds to its status
+            idList: lists[project_data.project_status.value] ? lists[project_data.project_status.value] : lists["Other"]
+        };
+
+        // We are also going to adjust the labels of the card ; this requires a separate API call for labels to add or remove, so we prepare two lists    
+        var idLabels_add = [];
+        var idLabels_remove = [];
+
+        // As a first label to add, we want the project type to be in there anyway
+        idLabels_add.push(labels[project_data.project_type.value] ? labels[project_data.project_type.value].id : labels["Other"].id);
+
+        // We are also going to add a comment in the card to track the changes made since the last update
+        var comment = "";
+
+        if (project_data.project_status.changed) {
+            comment += "Updated status: " + project_data.project_status.value;
+            comment += " (was: " + project_data.project_status.previous + ")";
+            comment += "%0D%0A";
+        }
+
+        // Project dates
+        var datechanged = false;
+        var datemissing = false;
+
+        if (project_data.project_type.value !== "SLA") { // Date labels are irrelevant for SLAs
+            
+            if (project_data.start_date.value == null || project_data.start_date.value.substring(0, 10) == "0000-00-00") datemissing = true;
+            else {
+
+                if (project_data.start_date.changed) {
+                    comment += "Start date: " + project_data.start_date.value.substring(0, 10);
+                    comment += " (was: " + project_data.start_date.previous.substring(0, 10) + ")";
+                    comment += "%0D%0A";
+                    datechanged = true;
+                }
+
+            }
+
+            if (project_data.end_impl.value == null || project_data.end_impl.value.substring(0, 10) == "0000-00-00") datemissing = true;
+            else {
+
+                if (project_data.end_impl.changed) {
+                    comment += "End implementation date: " + project_data.end_impl.value.substring(0, 10);
+                    comment += " (was: " + project_data.end_impl.previous.substring(0, 10) + ")";
+                    comment += "%0D%0A";
+                    datechanged = true;
+                }
+
+            }
+
+            if (project_data.start_test.value == null || project_data.start_test.value.substring(0, 10) == "0000-00-00") datemissing = true;
+            else {
+                if (project_data.start_test.changed) {
+                    comment += "Start test date: " + project_data.start_test.value.substring(0, 10);
+                    comment += " (was: " + project_data.start_test.previous.substring(0, 10) + ")";
+                    comment += "%0D%0A";
+                    datechanged = true;
+                }
+            }
+
+            if (project_data.end_date.value == null || project_data.end_date.value.substring(0, 10) == "0000-00-00") datemissing = true;
+            else {
+
+                if (project_data.end_date.changed) {
+                    comment += "End date: " + project_data.end_date.value.substring(0, 10);
+                    comment += " (was: " + project_data.end_date.previous.substring(0, 10) + ")";
+                    comment += "%0D%0A";
+                    datechanged = true;
+                }
+
+            }
+        
+
+            if (datechanged) idLabels_add.push(labels["Date changed"].id);
+            if (datemissing) idLabels_add.push(labels["Date missing"].id);
+            else {
+                idLabels_remove.push(labels["Date missing"].id);
+
+                var nextDeadline;
+
+                if (project_data.project_status.value == "In Planning" || project_data.project_status.value == "In Progress") nextDeadline = new Date(project_data.end_impl.value.substring(0, 10));
+                else nextDeadline = new Date(project_data.end_date.value.substring(0, 10));
+
+                if (nextDeadline < new Date()) idLabels_add.push(labels["Outdated"].id);
+                else idLabels_remove.push(labels["Outdated"].id);
+            }
+        }
+        // Project time & budget
+
+        if (project_data.billable_hours.changed) {
+            comment += "Billable hours updated from " + project_data.billable_hours.previous + " to " + project_data.billable_hours.value;
+            comment += "%0D%0A";
+            idLabels_add.push(labels["Billable changed"].id);
+        }
+
+
+        if (project_data.worked_hours.value > 0.0) {
+            comment += (project_data.worked_hours.changed ? (project_data.worked_hours.value - project_data.worked_hours.previous) : 0) + " hour(s) worked last week.";
+        }
+        
+        if (project_data.project_type.value == "Module installation" || project_data.project_type.value == "Fixed Price Project") {
+            var percentage = project_data.worked_hours.value / project_data.billable_hours.value;
+            var budgetrisk = false;
+
+            if (project_data.project_status.value == "In Planning" && percentage > 0.1) budgetrisk = true;
+            if (project_data.project_status.value  == "In Progress" && percentage > 0.6) budgetrisk = true;
+            if (project_data.project_status.value  == "In Test" && percentage > 0.8) budgetrisk = true;
+
+            if (budgetrisk) idLabels_add.push(labels["Budget risk"].id);
+            else idLabels_remove.push(labels["Budget risk"].id);
+        }
+
+
+        var action = 'PUT';
+        var url = "https://api.trello.com/1/cards/" + card_id + "?";
+
+        for (var c in card) {
+            url += c + "=" + card[c] + "&";
+        }
+
+        url += "pos=top";
+
+        // Update card list and labels
+        var request = new XMLHttpRequest();
+
+        request.open(action, url);
+
+        request.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                
+                if (comment.length > 0 && !isNewCard) createComment(card_id, comment, settings.tkey, settings.ttoken);
+                if (idLabels_add.length > 0) addLabels(idLabels_add, card_id, settings.tkey, settings.ttoken);
+                if (idLabels_remove.length > 0) removeLabels(idLabels_remove, card_id, settings.tkey, settings.ttoken);                        
+                //var response = JSON.parse(request.responseText);
+                resolve(project_data);
+            } 
+        };
+
+        request.send();
+
+        // Comment card
+
+
+    })]);
 }
 
 function updateCard(t, card_id, pid, settings, labels) {
